78277921d65488bc7979b2788fc3f775a7e6b683
Manish Shrestha <manish.sh2071@gmail.com>
2019-04-17 18:35:43 +0545

    Initial commit

---
+++ app/src/main/java/com/example/webbrowser/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.webbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etUrl;
+    private Button btnGo;
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnGo=findViewById(R.id.btnGo);
+        etUrl=findViewById(R.id.etUrl);
+        webView=findViewById(R.id.webView);
+
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url="https://www."+etUrl.getText().toString();
+
+                webView.getSettings().setLoadsImagesAutomatically(true);
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+                webView.loadUrl(url);
+            }
+        });
+    }
+
+}
